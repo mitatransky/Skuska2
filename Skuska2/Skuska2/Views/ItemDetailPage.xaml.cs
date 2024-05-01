5de186a0b7a3bbe35f42ffa0a9a762cc84ec525c
mitat <mitat@MISONOV>
2024-05-01 20:30:12 +0200

    Add project files.

---
+++ Skuska2/Skuska2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Skuska2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Skuska2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Skuska2/Skuska2/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Skuska2.Models;
+using Skuska2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Skuska2.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
